40a3e35b08d6cc464745f8aadcbc5bfec427a9af
uguumur <uguumur.oldokh@gmail.com>
2022-03-22 15:08:36 +0800

    blog post

--- pages/home.js
+++ pages/home.js
@@ -15,7 +15,72 @@ const navigation = [
 
 const Home = () => {
   const [profile, setProfile] = useState();
+  const [title, setTitle] = useState();
+  const [message, setMessage] = useState();
+  const [image, setImage] = useState();
+  const [news, setNews] = useState([]);
+
   const router = useRouter();
+
+  function getBase64(file) {
+    return new Promise((res, rej) => {
+      let reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = function () {
+        res(reader.result);
+      };
+      reader.onerror = function (error) {
+        rej(error);
+      };
+    });
+  }
+
+  async function postNews() {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: JSON.stringify({
+        title,
+        message,
+        image: await getBase64(image),
+        date: moment().format("YYYY-MM-dd HH:mm:ss"),
+      }),
+    };
+
+    fetch("http://localhost:8080/api/blog", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.message == "success") {
+          getNews();
+        }
+      })
+      .catch((error) => console.log("error", error));
+  }
+
+  async function getNews() {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("token", window.localStorage.getItem("token"));
+
+    var requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+    };
+
+    fetch("http://localhost:8080/api/blog", requestOptions)
+      .then((response) => {
+        if (response.status == 401) {
+          router.push("/");
+        } else {
+          return response.json();
+        }
+      })
+      .then((result) => setNews(result.data))
+      .catch((error) => console.log("errorsdfsdfsdf", error));
+  }
 
   useEffect(() => {
     const token = window.localStorage.getItem("token");
@@ -31,11 +96,13 @@ const Home = () => {
       setProfile("Admin");
     }
     console.log("Token: ", token, tokenExpTime);
+
+    getNews();
   }, []);
 
   return (
     <>
-      <div className="relative bg-white overflow-hidden">
+      <div className="relative bg-white">
         <div className="max-w-7xl mx-auto">
           <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
             <svg
@@ -141,48 +208,69 @@ const Home = () => {
                 </Popover.Panel>
               </Transition>
             </Popover>
-
-            <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-              <div className="sm:text-center lg:text-left">
-                <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                  <span className="block xl:inline">Data to enrich your</span>{" "}
-                  <span className="block text-indigo-600 xl:inline">
-                    online business
-                  </span>
-                </h1>
-                <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
-                  qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
-                  occaecat fugiat aliqua.
-                </p>
-                <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                  <div className="rounded-md shadow">
-                    <a
-                      href="#"
-                      className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
-                    >
-                      Get started
-                    </a>
-                  </div>
-                  <div className="mt-3 sm:mt-0 sm:ml-3">
-                    <a
-                      href="#"
-                      className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
-                    >
-                      Live demo
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </main>
           </div>
-        </div>
-        <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-          <img
-            className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-            src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
-            alt=""
-          />
+          <main className="mt-10 mx-auto">
+            <div className="sm:text-center lg:text-left">
+              <h1 className="font-bold text-3xl">Blog post</h1>
+              <div className="flex flex-col">
+                <input
+                  type="text"
+                  defaultValue={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  placeholder="Title"
+                  className="mt-6"
+                />
+
+                <textarea
+                  defaultValue={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  className="
+                    mt-4
+                    form-control
+                    block
+                    w-full
+                    px-3
+                    py-1.5
+                    text-base
+                    font-normal
+                    text-gray-700
+                    bg-white bg-clip-padding
+                    border border-solid border-gray-300
+                    rounded
+                    transition
+                    ease-in-out
+                    m-0
+                  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  id="exampleFormControlTextarea1"
+                  rows="3"
+                  placeholder="Your message"
+                ></textarea>
+                <input
+                  type="file"
+                  accept="image/png, image/gif, image/jpeg"
+                  placeholder=""
+                  defaultValue={image}
+                  onChange={(e) => setImage(e.target.files[0])}
+                  className="mt-6"
+                />
+                <button
+                  onClick={() => postNews()}
+                  className="bg-indigo-600 text-white pt-2 pb-2 w-32 rounded mt-6"
+                >
+                  post
+                </button>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 gap-4 mt-12">
+              {news.map((x) => (
+                <div className="bg-white shadow-lg w-full h-40 p-5">
+                  <h2 className="font-bold text-3xl">{x.title}</h2>
+                  <p>{x.message}</p>
+                  <img src={x.image} alt={x.title} />
+                </div>
+              ))}
+            </div>
+          </main>
         </div>
       </div>
     </>
